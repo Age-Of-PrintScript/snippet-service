68ec6f5091b96832cdb323636e37758787f41f9c
Facugarciar0805 <facugarciar@gmail.com>
2026-09-22 20:36:54 -0300

    initial commit
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

---
+++ src/test/kotlin/age/of/printscript/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package age.of.printscript.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
